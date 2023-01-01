7358f970b10c97f7d9abf5a8d5fbe54a8904d937
IBuildGamesSometimes <103070755+IBuildGamesSometimes@users.noreply.github.com>
2023-01-01 17:16:46 +0200

    a

--- Main.lua
+++ Main.lua
@@ -1,10 +1,6 @@
 local module = {}
 
-local Modules = {
-	"TestModule"
-}
-
-function module.Start()
+function module.Start(Modules)
     for _,Module in pairs(Modules) do
         module[Module] = loadstring(game:HttpGet('https://raw.githubusercontent.com/IBuildGamesSometimes/RGS2/main/Modules/'..Module..'.lua'),Module)()
     end
