4df34f45b128b5bfb7f58ea47cf0999c85e54385
IBuildGamesSometimes <103070755+IBuildGamesSometimes@users.noreply.github.com>
2023-01-01 19:17:54 +0200

    lol I hate lua

--- Modules/Sound.lua
+++ Modules/Sound.lua
@@ -3,10 +3,10 @@ local SoundService = game:GetService("SoundService")
 local module = {}
 
 local Sounds = {
-    Click = {"Id" = 876939830}, -- By @IcyTea
-    Win = {"Id" = 576560247}, -- By @Iighthour
-    Start = {"Id" = 499381494}, -- By @imdone13
-    Error = {"Id" = 550209561}, -- By @Fierzaa
+    Click = {Id = 876939830}, -- By @IcyTea
+    Win = {Id = 576560247}, -- By @Iighthour
+    Start = {Id = 499381494}, -- By @imdone13
+    Error = {Id = 550209561}, -- By @Fierzaa
     -- All sounds will be replaced with sounds by @Roblox in the future
 }
 
@@ -14,7 +14,7 @@ function module.Play(Sound)
     if Sounds[Sound] == nil or Sounds[Sound].Instance == nil then
         local Instance = Instance.new("Sound")
         Instance.SoundId = "rbxassetid://"..tostring(Sound)
-        Sounds[Sound] = {"Id" = Sound, "Instance" = Instance}
+        Sounds[Sound] = {Id = Sound, Instance = Instance}
     end
     SoundService:PlayLocalSound(Sounds[Sound].Instance)
 end
