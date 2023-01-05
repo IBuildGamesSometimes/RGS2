15c41fae446da2ad4b1859689af3a5011abac214
IBuildGamesSometimes <103070755+IBuildGamesSometimes@users.noreply.github.com>
2023-01-05 18:30:46 +0200

    a

--- Modules/File.lua
+++ Modules/File.lua
@@ -4,7 +4,7 @@ local module = {}
 
 function module.Read(File)
     if isfile(game.GameId.."/"..File..".json") then
-        return HttpService:JSONDecode(readfile(game.GameId.."/"..File..".json"))
+        return readfile(game.GameId.."/"..File..".json")
     else
         warn("File.Read Error: Unknown File "..File)
     end
@@ -14,12 +14,12 @@ function module.Write(File, Data)
     if isfolder(game.GameId) == false then
         makefolder(game.GameId)
     end
-    writefile(game.GameId.."/"..File..".json",HttpService:JSONEncode(Data))
+    writefile(game.GameId.."/"..File..".json",Data)
 end
 
 function module.Append(File, Data)
     if isfile(game.GameId.."/"..File..".json") then
-        appendfile(game.GameId.."/"..File..".json",HttpService:JSONEncode(Data))
+        appendfile(game.GameId.."/"..File..".json",Data)
     else
         module.Write(File,Data)
     end
