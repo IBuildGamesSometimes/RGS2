d8d2ebe546769796145d2071730ceff83e45b6df
IBuildGamesSometimes <103070755+IBuildGamesSometimes@users.noreply.github.com>
2023-01-19 15:57:07 +0200

    Fixed PlaceId problem

--- Modules/File.lua
+++ Modules/File.lua
@@ -3,31 +3,31 @@ local HttpService = game:GetService("HttpService")
 local module = {}
 
 function module.Read(File)
-    if isfile(game.GameId.."/"..File..".json") then
-        return readfile(game.GameId.."/"..File..".json")
+    if isfile(game.PlaceId.."/"..File..".json") then
+        return readfile(game.PlaceId.."/"..File..".json")
     else
         warn("File.Read Error: Unknown File "..File)
     end
 end
 
 function module.Write(File, Data)
-    if isfolder(game.GameId) == false then
-        makefolder(game.GameId)
+    if isfolder(game.PlaceId) == false then
+        makefolder(game.PlaceId)
     end
-    writefile(game.GameId.."/"..File..".json",Data)
+    writefile(game.PlaceId.."/"..File..".json",Data)
 end
 
 function module.Append(File, Data)
-    if isfile(game.GameId.."/"..File..".json") then
-        appendfile(game.GameId.."/"..File..".json",Data)
+    if isfile(game.PlaceId.."/"..File..".json") then
+        appendfile(game.PlaceId.."/"..File..".json",Data)
     else
         module.Write(File,Data)
     end
 end
 
 function module.Delete(File)
-    if isfile(game.GameId.."/"..File..".json") then
-        delfile(game.GameId.."/"..File..".json")
+    if isfile(game.PlaceId.."/"..File..".json") then
+        delfile(game.PlaceId.."/"..File..".json")
     else
         warn("File.Delete Error: Unknown File "..File)
     end
